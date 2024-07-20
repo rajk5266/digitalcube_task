f024145c747b5dd5745774c1a399e5d871f32740
rajk5266 <kushwaharaj903@gmail.com>
2024-07-21 01:41:01 +0530

    image path

--- src/utils/weatherIcons.js
+++ src/utils/weatherIcons.js
@@ -1,14 +1,14 @@
 
 const weatherIcons = {
-    Clear: "/assets/icons/clear.png",
-    Clouds: "/assets/icons/clouds.png",
-    Rain: "/assets/icons/rain.png",
-    Atmosphere: "/assets/icons/cloudy.png",
-    Haze: "/assets/icons/haze.png",
-    Snow: "/assets/icons/snow.png",
-    Drizzle: "/assets/icons/drizzle.png",
-    Thunderstorm: '/assets/icons/thunderstorm.png',
-    Mist: '/assets/icons/cloudy.png'
+    Clear: "../assets/icons/clear.png",
+    Clouds: "../assets/icons/clouds.png",
+    Rain: "../assets/icons/rain.png",
+    Atmosphere: "../assets/icons/cloudy.png",
+    Haze: "../assets/icons/haze.png",
+    Snow: "../assets/icons/snow.png",
+    Drizzle: "../assets/icons/drizzle.png",
+    Thunderstorm: '../assets/icons/thunderstorm.png',
+    Mist: '../assets/icons/cloudy.png'
 
 
   };
